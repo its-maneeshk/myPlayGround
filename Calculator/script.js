7441d0038ecc9950235436b7de0f7d9dc12bbd80
Manish Kumar Patel <maneeshkurmii@gmail.com>
2024-12-20 14:35:29 +0530

    day second of updating calculator functionalities

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,15 +1,55 @@
-let initialValue = '';
+let initialValue = 0;
 
 function appendNumber(number) {
-    if(initialValue === '0') {
-        initialValue = number.toSting();
+    if (initialValue === 0 || initialValue === '') {
+        initialValue = number.toString();
     } else {
         initialValue += number.toString();
     }
     displayResult();
 }
 
+function appendOperator(operator) {
+    initialValue += operator;
+    displayResult();
+}
 
+function appendDecimal(decimal) {
+    initialValue += decimal;
+    displayResult();
+}
+
+function clearScreen() {
+    initialValue = '';
+    displayResult();
+}
+
+function backSpace() {
+    if (initialValue === '') {
+        const messageElement = document.getElementById('errorMessage');
+        messageElement.innerText = "Nothing to clear in screen";
+        messageElement.style.color = 'crimson';
+        messageElement.style.textAlign = 'right';
+        messageElement.style.backgroundColor = 'yellow';
+
+        setTimeout(() => {
+            messageElement.innerText = '';
+            messageElement.style = '';
+        }, 900);
+    } else {
+        initialValue = initialValue.slice(0, -1);
+    }
+    displayResult();
+}
+
+function calculateResult() {
+    const resultStore = eval(initialValue);
+    initialValue = resultStore;
+    displayResult();
+}
 function displayResult() {
-    document.getElementById('displayAnswer').value = initialValue;
+    const finalResultStore = document.getElementById('answerScreen');
+
+    finalResultStore.value = initialValue;
+    finalResultStore.style.color = 'crimson';
 }
